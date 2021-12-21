0685a5224df36b2375af33e3c8aa271ce8c0a1f9
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2021-12-21 23:26:29 +0530

    Update HelloWorld.go

--- goworkspace/src/HelloWorld/HelloWorld.go
+++ goworkspace/src/HelloWorld/HelloWorld.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 
-	fmt.Println("Hello World !!")
+	n, _ := fmt.Println("Hello World !!")
+	fmt.Println(n)
+
+	sayHello()
+
+	for i := 0; i <= 10; i++ {
+		n, err := fmt.Println("Number: ", i, "; also boolean: ", true)
+		fmt.Println(n, err)
+	}
+}
+
+func sayHello() {
+	fmt.Println("Hello from sayHello()")
 }
