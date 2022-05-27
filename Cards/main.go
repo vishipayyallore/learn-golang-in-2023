3b05b11c77ee8dd16a9ab6af63db5811f74e296f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-27 08:55:41 +0530

    first program

--- Cards/main.go
+++ Cards/main.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 
 	card := getNewCard()
-
 	fmt.Println(card)
+
+	cards := []string{"Ace of Dimonds", getNewCard()}
+	fmt.Println(cards)
+
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func getNewCard() string {
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-golang-in-2022
 I am learning Go Lang in 2020 using multiple Books, Video Courses, and Web Sites
+
+```golang
+go mod init Cards
+go mod tidy
+```
