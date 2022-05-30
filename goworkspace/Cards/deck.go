4349d21d594af5f8b71a3b623679897c6ffec10e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-30 17:18:03 +0530

    Update to Program

--- goworkspace/Cards/deck.go
+++ goworkspace/Cards/deck.go
@@ -1,3 +1,13 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+func (this deck) print() {
+
+	for i, card := range this {
+		fmt.Println(i, card)
+	}
+
+}
--- goworkspace/Cards/main.go
+++ goworkspace/Cards/main.go
@@ -7,15 +7,18 @@ func main() {
 	card := getNewCard()
 	fmt.Println(card)
 
-	cards := []string{"Ace of Dimonds", getNewCard()}
-	fmt.Println(cards)
+	cards := deck{"Ace of Dimonds", getNewCard()}
+	// fmt.Println(cards)
+	// cards.print()
 
 	cards = append(cards, "Six of Spades")
-	fmt.Println(cards)
+	// fmt.Println(cards)
+	// cards.print()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	cards.print()
 }
 
 func getNewCard() string {
