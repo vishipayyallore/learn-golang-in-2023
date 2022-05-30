69c26b7bbf9134b5fc3649567991cfbf2f7d7f67
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-30 19:19:25 +0530

    Update main.go

--- goworkspace/Cards/main.go
+++ goworkspace/Cards/main.go
@@ -3,20 +3,12 @@ package main
 func main() {
 
 	var card = singleCard(getNewCard())
-	// fmt.Println(card)
 	card.print()
 
 	cards := deck{"Ace of Dimonds", getNewCard()}
-	// fmt.Println(cards)
-	// cards.print()
 
 	cards = append(cards, "Six of Spades")
-	// fmt.Println(cards)
-	// cards.print()
 
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
 	cards.print()
 }
 
