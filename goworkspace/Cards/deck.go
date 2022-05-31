4b89142172e25900a51544ef58cff22ea4c9946e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-01 00:45:57 +0530

    Update deck.go

--- goworkspace/Cards/deck.go
+++ goworkspace/Cards/deck.go
@@ -6,7 +6,18 @@ type singleCard string
 type deck []string
 
 func newDeck() deck {
+	cards := deck{}
 
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
 }
 
 func (this deck) print() {
