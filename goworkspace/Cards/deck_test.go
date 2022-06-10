e132089cc30d31a5bb62917c79b204aedc4bc6be
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-10 11:31:41 +0530

    Update deck_test.go

--- goworkspace/Cards/deck_test.go
+++ goworkspace/Cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -20,4 +23,26 @@ func TestNewDeck(t *testing.T) {
 	if cards[len(cards)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", cards[len(cards)-1])
 	}
+}
+
+func TestSaveToFileAndGetNewDeckFromFile(t *testing.T) {
+	filename := "./Data/_testdeskofcards.txt"
+
+	os.Remove(filename)
+
+	cards := getNewDeck()
+	cards.saveToFile(filename)
+
+	loadedDeck := getNewDeckFromFile(filename)
+
+	expected := 16
+	actual := len(loadedDeck)
+
+	if actual != expected {
+		t.Errorf("Expected deck length of %v, but got %v", expected, actual)
+	}
+
+	loadedDeck.print("Deck from File")
+
+	os.Remove(filename)
 }
