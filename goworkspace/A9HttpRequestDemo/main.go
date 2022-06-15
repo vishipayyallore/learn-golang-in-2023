c565f3dab9d8f3c48c2b91228fb3d6891257ae0f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-15 12:26:35 +0530

    Update main.go

--- goworkspace/A9HttpRequestDemo/main.go
+++ goworkspace/A9HttpRequestDemo/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWritter struct{}
 
 func main() {
 
@@ -22,9 +25,23 @@ func main() {
 	fmt.Println("Output Received: ", resp)
 	fmt.Println("Output Received: ", resp.Body)
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println("Output Received: ", string(bs))
+	// Readers are the most common way to read data from a response body.
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println("Output Received: ", string(bs))
+
+	// Writers are the most common way to write data to a response body.
+	// io.Copy(os.Stdout, resp.Body)
+
+	lw := logWritter{}
+	io.Copy(lw, resp.Body)
 
 	defer resp.Body.Close()
+}
+
+func (logWritter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
