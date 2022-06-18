edd52c9e249820f25c7000d25224e7cbb19e1ab5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-18 16:08:10 +0530

    Added Channels

--- goworkspace/A12ChannelsDemo/main.go
+++ goworkspace/A12ChannelsDemo/main.go
@@ -15,18 +15,25 @@ func main() {
 		"http://www.amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down !")
+		c <- fmt.Sprintf("%s might be down", link)
+
 		return
 	}
 
 	fmt.Println(link, "is up !!")
+	c <- fmt.Sprintf("%s is up !!", link)
 }
