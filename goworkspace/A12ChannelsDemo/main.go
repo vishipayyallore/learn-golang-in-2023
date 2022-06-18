e74fc63cd0efff7b4e9675f403472af3c0b686de
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-18 16:27:08 +0530

    Update main.go

--- goworkspace/A12ChannelsDemo/main.go
+++ goworkspace/A12ChannelsDemo/main.go
@@ -21,16 +21,9 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	// // Each message will be received in each print statement
-	// fmt.Println(<-c)
-	// fmt.Println(<-c)
-	// fmt.Println(<-c)
-	// fmt.Println(<-c)
-	// fmt.Println(<-c)
-
 	// // All messages will be received in one print statement
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 
 }
@@ -39,12 +32,19 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
-		// fmt.Println(link, "might be down !")
-		c <- fmt.Sprintf("%s might be down", link)
+		fmt.Println(link, "might be down !")
+		c <- link
 
 		return
 	}
 
-	// fmt.Println(link, "is up !!")
-	c <- fmt.Sprintf("%s is up !!", link)
+	fmt.Println(link, "is up !!")
+	c <- link
 }
+
+// // Each message will be received in each print statement
+// fmt.Println(<-c)
+// fmt.Println(<-c)
+// fmt.Println(<-c)
+// fmt.Println(<-c)
+// fmt.Println(<-c)
