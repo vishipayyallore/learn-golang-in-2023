f2dd9db36b9dc132fc77b452072bff87cef29b26
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-30 12:28:09 +0530

    Few Changes

--- goworkspace/B2HelloWorld/hello_test.go
+++ goworkspace/B2HelloWorld/hello_test.go
@@ -3,13 +3,32 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sri Varu")
+		want := "Hello, Sri Varu"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	// got := Hello("Sri Varu")
+	// want := "Hello, Sri Varu"
+
+	// if got != want {
+	// 	t.Errorf("got %q want %q", got, want)
+	// }
 }
 
-// Additional Notes: 
+// Additional Notes:
 // t.Fail() is a function that can be called to indicate that a test has failed.
