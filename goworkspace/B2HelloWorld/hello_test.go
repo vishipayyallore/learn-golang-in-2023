c976b72aad46ff68233a4e551783d7c4a0b8eda0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-04 20:41:59 +0530

    Added Spanish Language

--- goworkspace/B2HelloWorld/hello_test.go
+++ goworkspace/B2HelloWorld/hello_test.go
@@ -4,31 +4,43 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Sri Varu")
-		want := "Hello, Sri Varu"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		t.Helper()
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Sri Varu", "English")
+		want := "Hello, Sri Varu"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
-	// got := Hello("Sri Varu")
-	// want := "Hello, Sri Varu"
-
-	// if got != want {
-	// 	t.Errorf("got %q want %q", got, want)
-	// }
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 // Additional Notes:
 // t.Fail() is a function that can be called to indicate that a test has failed.
+
+// got := Hello("Sri Varu")
+// want := "Hello, Sri Varu"
+
+// if got != want {
+// 	t.Errorf("got %q want %q", got, want)
+// }
--- goworkspace/B2HelloWorld/main.go
+++ goworkspace/B2HelloWorld/main.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Sri Varu"))
+	fmt.Println(Hello("Sri Varu", ""))
 }
