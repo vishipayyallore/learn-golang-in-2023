3585ca770608262ab8cbe99fa3f64a89e3055e41
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-25 22:18:17 +0530

    Update main.go

--- learngolang2023/a1-helloworld/main.go
+++ learngolang2023/a1-helloworld/main.go
@@ -5,11 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("😍 ❤ 🤦‍♀️ 🎂 🎉 🎈 🎁 🎊 🎀 🎁 🎈 🎉 🎂 🤦‍♀️ ❤")
 	fmt.Println(`
-	A Necessary Autumn Inside Each
-	~Rumi~
+	Hello World
+	~GoLang~
 
-	You and I have spoken all these words,
-	but as for the way we have to go,
-	words are no preparation.
+	Multi line comment.
 	`)
 }
