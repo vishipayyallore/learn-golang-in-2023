a7083fd13c4410191c8791f4c1a8b1783e904106
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-27 23:19:26 +0530

    Update main.go

--- learngolang2023/a4-print-binary-hexadecimal/main.go
+++ learngolang2023/a4-print-binary-hexadecimal/main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 	adams := 42
-	fmt.Printf("42 as binary, %b \n", adams)
-	fmt.Printf("42 as hexadecimal, %x \n", adams)
+	fmt.Printf("%v as binary, %#b \n", adams, adams)
+	fmt.Printf("%v as hexadecimal, %#x \n", adams, adams)
 
 	// print these values as both binary & hexadecimal
-	// a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", a, a, a)
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", b, b, b)
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", c, c, c)
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", d, d, d)
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", e, e, e)
+	fmt.Printf("Decimal: %v \tBinary: %#b \tHexadecimal: %#x \n", f, f, f)
+
 }
