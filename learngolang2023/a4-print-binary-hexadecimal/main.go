72aa49295cdd15a57e3bb60c3887f37cb92e0fe2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-27 23:15:25 +0530

    Create main.go

---
+++ learngolang2023/a4-print-binary-hexadecimal/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	adams := 42
+	fmt.Printf("42 as binary, %b \n", adams)
+	fmt.Printf("42 as hexadecimal, %x \n", adams)
+
+	// print these values as both binary & hexadecimal
+	// a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+}
