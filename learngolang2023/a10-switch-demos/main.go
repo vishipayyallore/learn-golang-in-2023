d18c557bf68c7bb220fac375b9c504a5c0ab6e6e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-12 23:00:51 +0530

    Update main.go

--- learngolang2023/a10-switch-demos/main.go
+++ learngolang2023/a10-switch-demos/main.go
@@ -11,6 +11,8 @@ func main() {
 	showCommasSeparateMultipleExpressionsDemo()
 
 	showSwitchWithoutExpressionDemo()
+
+	showTypeSwitchDemo()
 }
 
 // basic switch
@@ -52,4 +54,25 @@ func showSwitchWithoutExpressionDemo() {
 	default:
 		fmt.Println("It's after noon", t.Hour())
 	}
+}
+
+// A type switch compares types instead of values. You can use this to discover the type of an interface value.
+// In this example, the variable t will have the type corresponding to its clause.
+func showTypeSwitchDemo() {
+	fmt.Println("\n***** type switch *****")
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
