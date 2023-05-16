dec6bd985370c6afa137da57ba3300ae6763ae30
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-16 23:18:37 +0530

    Update main.go

--- learngolang2023/a11-array-demos/main.go
+++ learngolang2023/a11-array-demos/main.go
@@ -2,77 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	showBasicSwitchDemo()
 
-	showCommasSeparateMultipleExpressionsDemo()
+	var a [5]int
+	fmt.Println("emp:", a)
 
-	showSwitchWithoutExpressionDemo()
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-	showTypeSwitchDemo()
-}
+	fmt.Println("len:", len(a))
 
-// basic switch
-func showBasicSwitchDemo() {
-	fmt.Println("***** basic switch *****")
-	i := 2
-	fmt.Print("Write ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-	}
-}
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
 
-// Commas to separate multiple expressions in the same case statement and optional default case as well.
-func showCommasSeparateMultipleExpressionsDemo() {
-	fmt.Println("\n***** Commas to separate multiple expressions in the same case statement *****")
-
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend")
-	default:
-		fmt.Println("It's a weekday")
-	}
-}
-
-// switch without an expression is an alternate way to express if/else logic.
-// Here we also show how the case expressions can be non-constants.
-func showSwitchWithoutExpressionDemo() {
-	fmt.Println("\n***** switch without an expression *****")
-
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("It's before noon ", t.Hour())
-	default:
-		fmt.Println("It's after noon", t.Hour())
-	}
-}
-
-// A type switch compares types instead of values. You can use this to discover the type of an interface value.
-// In this example, the variable t will have the type corresponding to its clause.
-func showTypeSwitchDemo() {
-	fmt.Println("\n***** type switch *****")
-
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("The type is %T\n", t)
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
 		}
 	}
-
-	whatAmI(true)
-	whatAmI(1)
-	whatAmI("hey")
+	fmt.Println("2d: ", twoD)
 }
