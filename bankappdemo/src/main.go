8225b7fb5d6ab998bee74d6f8637e2419ab0e06f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-01 20:58:22 +0530

    Moved the file

--- bankappdemo/src/main.go
+++ bankappdemo/src/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
