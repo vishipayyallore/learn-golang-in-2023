27dac53227f6498f5058e5c8d9c5e0c86562ae6b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-02 19:54:48 +0530

    Code Refactoring

--- bankappdemo/src/app/appserver.go
+++ bankappdemo/src/app/appserver.go
@@ -23,9 +23,3 @@ func StartBankServer() {
 	log.Fatal(http.ListenAndServe(hostServer, nil))
 
 }
-
-type CustomerV2 struct {
-	Name    string `json:"full_name" xml:"full_name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zip_code"`
-}
--- bankappdemo/src/main.go
+++ bankappdemo/src/main.go
@@ -11,8 +11,5 @@ func main() {
 	customer := app.Customer{Name: "John", City: "New York", Zipcode: "12345"}
 	fmt.Println(customer)
 
-	customer2 := app.CustomerV2{Name: "Jean", City: "New Jersey", Zipcode: "23456"}
-	fmt.Println(customer2)
-
 	app.StartBankServer()
 }
