086b689f93251fd3645d6c4d6f65ee8700608159
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-02 22:23:20 +0530

    Update appserver.go

--- bankappdemo/src/app/appserver.go
+++ bankappdemo/src/app/appserver.go
@@ -9,17 +9,19 @@ import (
 func StartBankServer() {
 	hostServer := "localhost:8080"
 
+	newServerMux := http.NewServeMux()
+
 	// Defining the routes
-	http.HandleFunc("/", ApiRootHandler)
-	http.HandleFunc("/api", ApiRootHandler)
+	newServerMux.HandleFunc("/", ApiRootHandler)
+	newServerMux.HandleFunc("/api", ApiRootHandler)
 
 	// Defining the routes for Customers
-	http.HandleFunc("/api/customers", GetAllCustomersHandler)
-	http.HandleFunc("/api/customersjson", GetAllCustomersInJsonHandler)
-	http.HandleFunc("/api/customersxml", GetAllCustomersInXmlHandler)
+	newServerMux.HandleFunc("/api/customers", GetAllCustomersHandler)
+	newServerMux.HandleFunc("/api/customersjson", GetAllCustomersInJsonHandler)
+	newServerMux.HandleFunc("/api/customersxml", GetAllCustomersInXmlHandler)
 
 	// Starting the server
 	fmt.Println("Starting the Server on ", hostServer)
-	log.Fatal(http.ListenAndServe(hostServer, nil))
+	log.Fatal(http.ListenAndServe(hostServer, newServerMux))
 
 }
