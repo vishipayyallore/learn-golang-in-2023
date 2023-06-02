fce7681bbc60467c594ac884cbbf85e4790e9243
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-02 19:59:32 +0530

    Code Refactoring and Postman Collections

--- bankappdemo/src/app/customerHandlers.go
+++ bankappdemo/src/app/customerHandlers.go
@@ -14,12 +14,7 @@ type Customer struct {
 
 func GetAllCustomersHandler(w http.ResponseWriter, r *http.Request) {
 
-	customers := []Customer{
-		{Name: "John", City: "New York", Zipcode: "12345"},
-		{Name: "Jane", City: "New York", Zipcode: "12345"},
-		{Name: "Jack", City: "New York", Zipcode: "12345"},
-		{Name: "Jill", City: "New York", Zipcode: "12345"},
-	}
+	customers := GetDummyCustomers()
 
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Set("Content-Type", "application/xml")
@@ -32,12 +27,7 @@ func GetAllCustomersHandler(w http.ResponseWriter, r *http.Request) {
 
 func GetAllCustomersInJsonHandler(w http.ResponseWriter, r *http.Request) {
 
-	customers := []Customer{
-		{Name: "John", City: "New York", Zipcode: "12345"},
-		{Name: "Jane", City: "New York", Zipcode: "12345"},
-		{Name: "Jack", City: "New York", Zipcode: "12345"},
-		{Name: "Jill", City: "New York", Zipcode: "12345"},
-	}
+	customers := GetDummyCustomers()
 
 	w.Header().Set("Content-Type", "application/json")
 
@@ -46,14 +36,18 @@ func GetAllCustomersInJsonHandler(w http.ResponseWriter, r *http.Request) {
 
 func GetAllCustomersInXmlHandler(w http.ResponseWriter, r *http.Request) {
 
-	customers := []Customer{
+	customers := GetDummyCustomers()
+
+	w.Header().Set("Content-Type", "application/xml")
+
+	xml.NewEncoder(w).Encode(customers)
+}
+
+func GetDummyCustomers() []Customer {
+	return []Customer{
 		{Name: "John", City: "New York", Zipcode: "12345"},
 		{Name: "Jane", City: "New York", Zipcode: "12345"},
 		{Name: "Jack", City: "New York", Zipcode: "12345"},
 		{Name: "Jill", City: "New York", Zipcode: "12345"},
 	}
-
-	w.Header().Set("Content-Type", "application/xml")
-
-	xml.NewEncoder(w).Encode(customers)
 }
