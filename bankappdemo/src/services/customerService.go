a2f9ae849464ce3750f725baafb8db7e7ae54e1a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-03 10:47:25 +0530

    Update customerService.go

--- bankappdemo/src/services/customerService.go
+++ bankappdemo/src/services/customerService.go
@@ -1,1 +1,21 @@
 package services
+
+import (
+	"bankappdemo/domain"
+)
+
+type CustomerService interface {
+	GetAllCustomer(string) ([]domain.Customer, error)
+}
+
+type DefaultCustomerService struct {
+	repo domain.CustomerRepository
+}
+
+func (service DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, error) {
+	return service.repo.FindAll(status)
+}
+
+func NewCustomerService(repo domain.CustomerRepository) DefaultCustomerService {
+	return DefaultCustomerService{repo: repo}
+}
