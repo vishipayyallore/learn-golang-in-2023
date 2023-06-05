2726f971cd8e3da6640f2aec295e28aad00c082f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-05 17:10:58 +0530

    Update customerHandlers.go

--- bankappdemo/src/app/customerHandlers.go
+++ bankappdemo/src/app/customerHandlers.go
@@ -34,19 +34,20 @@ func (ch *CustomersHandlers) GetCustomer(w http.ResponseWriter, r *http.Request)
 	customer, err := ch.customerService.GetCustomer(customerId)
 
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(err.Code)
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(err.AsMessage())
+
 		return
 	}
 
 	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Set("Content-Type", "application/xml")
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/xml")
 		xml.NewEncoder(w).Encode(customer)
 	} else {
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customer)
 	}
 }
