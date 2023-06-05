8387476c9266841cef94e120498798e4be6b971a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-05 17:29:38 +0530

    Swamy/05jun 2023 (#18)
    * Update customerHandlers.go
    
    * Update customerHandlers.go
    
    * Update customerHandlers.go
    
    * Update errors.go
    
    * Update customerHandlers.go
    
    * Update customerHandlers.go

--- bankappdemo/src/app/customerHandlers.go
+++ bankappdemo/src/app/customerHandlers.go
@@ -20,10 +20,14 @@ func (ch *CustomersHandlers) GetAllCustomersHandler(w http.ResponseWriter, r *ht
 
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Set("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
+		if err := xml.NewEncoder(w).Encode(customers); err != nil {
+			panic(err)
+		}
 	} else {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
+		if err := json.NewEncoder(w).Encode(customers); err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -34,19 +38,21 @@ func (ch *CustomersHandlers) GetCustomer(w http.ResponseWriter, r *http.Request)
 	customer, err := ch.customerService.GetCustomer(customerId)
 
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(err.Code)
-		json.NewEncoder(w).Encode(err.AsMessage())
+		writeResponse(w, r, err.Code, err.AsMessage())
 		return
+	} else {
+		writeResponse(w, r, http.StatusOK, customer)
 	}
+}
 
+func writeResponse(w http.ResponseWriter, r *http.Request, statusCode int, data interface{}) {
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Set("Content-Type", "application/xml")
-		w.WriteHeader(http.StatusOK)
-		xml.NewEncoder(w).Encode(customer)
+		w.WriteHeader(statusCode)
+		xml.NewEncoder(w).Encode(data)
 	} else {
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(customer)
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(data)
 	}
 }
