8d02255d7c0a565e8e9203c9ff9e775f4de5b800
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-07 20:43:50 +0530

    Code Refactoring

--- bankappdemo/src/app/appserver.go
+++ bankappdemo/src/app/appserver.go
@@ -30,9 +30,14 @@ func StartBankServer() {
 	// customerHandlers := &CustomersHandlers{customerService: services.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	customersHandlersOld := &CustomersHandlersOld{customerService: services.NewCustomerService(domain.NewCustomerRepositoryDb())}
 
-	// Defining the routes for Customers
+	// Defining the routes for Customers Old
 	muxRouter.HandleFunc("/api/customersold", customersHandlersOld.GetAllCustomersHandler).Methods(http.MethodGet)
 	muxRouter.HandleFunc("/api/customersold/{customer_id:[0-9]+}", customersHandlersOld.GetCustomer).Methods(http.MethodGet)
+
+	customersHandlers := &CustomersHandlers{customerService: services.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	// Defining the routes for Customers Old
+	muxRouter.HandleFunc("/api/customers", customersHandlers.GetAllCustomersHandler).Methods(http.MethodGet)
+	muxRouter.HandleFunc("/api/customers/{customer_id:[0-9]+}", customersHandlers.GetCustomer).Methods(http.MethodGet)
 
 	// Starting the server
 	fmt.Println("Starting the Server on ", hostServer)
