7495e066eba6c9e986824dda56a6b712c31d05aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-09 14:16:18 +0530

    Update main.go

--- bankappdemo/src/main.go
+++ bankappdemo/src/main.go
@@ -3,14 +3,9 @@ package main
 import (
 	"bankappdemo/app"
 	"bankappdemo/logger"
-	"fmt"
 )
 
 func main() {
-	if logger.Logger == nil {
-		fmt.Println("logger.Logger is nil")
-	}
-
 	logger.Info("Starting appserver::StartBankServer()")
 
 	app.StartBankServer()
