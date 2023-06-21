d3e1ce23a83f3b7988e23fcc525f99c964c8ff73
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-21 11:04:42 +0530

    Code Refactoring

--- bankappdemo/src/app/appserver.go
+++ bankappdemo/src/app/appserver.go
@@ -36,19 +36,7 @@ func StartBankServer() {
 	muxRouter.HandleFunc("/", ApiRootHandler).Methods(http.MethodGet)
 	muxRouter.HandleFunc("/api", ApiRootHandler).Methods(http.MethodGet)
 
-	// Defining the routes for Demo Customers
-	muxRouter.HandleFunc("/api/democustomers", GetAllDemoCustomersHandler).Methods(http.MethodGet)
-	muxRouter.HandleFunc("/api/democustomersjson", GetAllDemoCustomersInJsonHandler).Methods(http.MethodGet)
-	muxRouter.HandleFunc("/api/democustomersxml", GetAllDemoCustomersInXmlHandler).Methods(http.MethodGet)
-
 	// Wiring up the Customer Handlers
-	// customerHandlers := &CustomersHandlers{customerService: services.NewCustomerService(domain.NewCustomerRepositoryStub())}
-	customersHandlersOld := &CustomersHandlersOld{customerService: services.NewCustomerService(domain.NewCustomerRepositoryDb())}
-
-	// Defining the routes for Customers Old
-	muxRouter.HandleFunc("/api/customersold", customersHandlersOld.GetAllCustomersHandler).Methods(http.MethodGet)
-	muxRouter.HandleFunc("/api/customersold/{customer_id:[0-9]+}", customersHandlersOld.GetCustomer).Methods(http.MethodGet)
-
 	customersHandlers := &CustomersHandlers{customerService: services.NewCustomerService(domain.NewCustomerRepositoryDb())}
 	// Defining the routes for Customers
 	muxRouter.HandleFunc("/api/customers", customersHandlers.GetAllCustomersHandler).Methods(http.MethodGet)
