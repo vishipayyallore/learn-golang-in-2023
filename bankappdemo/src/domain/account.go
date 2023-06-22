2bb57d79c17e64fa3b4e32379062298a548980f4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-22 20:47:44 +0530

    Account Repository

--- bankappdemo/src/domain/account.go
+++ bankappdemo/src/domain/account.go
@@ -1,5 +1,7 @@
 package domain
 
+import "bankappdemo/errs"
+
 type Account struct {
 	AccountId   string  `db:"account_id"`
 	CustomerId  string  `db:"customer_id"`
@@ -7,4 +9,10 @@ type Account struct {
 	AccountType string  `db:"account_type"`
 	Amount      float64 `db:"amount"`
 	Status      string  `db:"status"`
+}
+
+type AccountRepository interface {
+	Save(account Account) (*Account, *errs.AppError)
+	// SaveTransaction(transaction Transaction) (*Transaction, *errs.AppError)
+	// FindBy(accountId string) (*Account, *errs.AppError)
 }
