968ea16b7037035ae87e52e1d32f61c4eea2f6da
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-01 15:10:59 +0530

    Few Changes

---
+++ learngolang2023/a1-twonumberssum/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("😍 ❤ 🤦‍♀️ 🎂 🎉 🎈 🎁 🎊 🎀 🎁 🎈 🎉 🎂 🤦‍♀️ ❤")
+
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println("(❁´◡`❁)")
+
+	fmt.Println(`
+	Hello World
+	~GoLang~
+
+	Multi line string.
+	`)
+}
