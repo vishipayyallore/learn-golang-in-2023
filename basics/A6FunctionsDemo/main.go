cd5a20c47682b2f6e217913455d882b279e466cd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-13 12:45:49 +0530

    Update main.go

--- basics/A6FunctionsDemo/main.go
+++ basics/A6FunctionsDemo/main.go
@@ -12,8 +12,16 @@ func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 
 	fmt.Printf("PI Value is %g.\n", math.Pi)
+
+	num1 := 2
+	num2 := 3
+	fmt.Printf("Sum of %d and %d is %d.\n", num1, num2, addNumbers(num1, num2))
+
+	num1 = 5
+	num2 = 7
+	fmt.Printf("Sum of %d and %d is %d.\n", num1, num2, addNumbers(num1, num2))
 }
 
-func displayMessage(message string) {
-	fmt.Println(message)
+func addNumbers(x int, y int) int {
+	return x + y
 }
